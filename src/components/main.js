0a882e17b8bb0dfa52fb6eb66bfc58ed103bcea4
Pan Wangperawong <panthongw@gmail.com>
2016-03-15 14:04:13 -0400

    added search and pulling from iTunes api

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import $ from 'jquery';
+import SearchInput from './search_input';
 
 export default class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchResults: []
+    };
+    this.setSearchResults = (results) => this._setSearchResults(results);
+  }
+  _setSearchResults(results) {
+    this.setState({searchResults: results});
+  }
   render() {
+    let {searchResults} = this.state;
     return (
-      <div>Find me in src/components/main.js</div>
+      <div>
+        <SearchInput
+          setResults={this.setSearchResults}
+        />
+
+        <ul>
+        {searchResults.map((result, i) => {
+          return <li key={i}><img src={result.artworkUrl30}/></li>
+        })}
+        </ul>
+      </div>
     );
   }
 }
