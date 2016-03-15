4273f5afbdff5832cbfa3374e0584fb08fb81535
Pan Wangperawong <panthongw@gmail.com>
2016-03-14 20:47:25 -0400

    added testing setup confirmation test

---
+++ spec/setup_confirmation_test.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import TestUtils from "react-addons-test-utils"
+
+describe("Test Setup Confirmation", () => {
+	it('should just work', () => {
+		expect(true).toEqual(true);
+	});
+});
