d798838e03b2d3601c17259531aa1d5883ba152d
Pan Wangperawong <pwangperawong@gmail.com>
2016-06-21 15:39:00 -0400

    removed search related code, so it is more generalized

--- src/components/main.js
+++ src/components/main.js
@@ -9,18 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default class Main extends React.Component {
   constructor() {
     super();
-    this.state = {
-      searchResults: []
-    };
-    this.setSearchResults = (results) => this._setSearchResults(results);
-  }
-  componentDidMount() {
-  }
-  _setSearchResults(results) {
-    this.setState({searchResults: results});
   }
   render() {
-    let {searchResults} = this.state;
     return (
       <div>
         I work!
